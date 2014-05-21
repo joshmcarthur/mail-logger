b62739a1b52b780364d4f29d57fe0d2d9620a938
Josh McArthur <joshua.mcarthur@gmail.com>
2014-05-21 14:15:13 +1200

    Initialise log file before constructor

--- lib/mail/logger/logfile.rb
+++ lib/mail/logger/logfile.rb
@@ -2,9 +2,8 @@ class Mail::Logger::Logfile < ::Logger
   def initialize
     path = Mail::Logger.configuration.log_path
     filename =  Mail::Logger.configuration.log_file_name
-
-    super(File.open(File.join(path, filename), 'a+'))
     logfile = File.open(File.join(path, filename), 'a+')
     logfile.sync = true
+    super(logfile)
   end
 end
